f0ab748da01fa22c54c02830a4a914bfc92cb689
Mauro Villagra <maurovillagra257@gmail.com>
2023-10-16 16:39:32 -0300

    Termine el banner

--- src/components/home/banner/Banner.jsx
+++ src/components/home/banner/Banner.jsx
@@ -7,10 +7,10 @@ function Banner() {
       <div className="w-100 home_banner">
         <div className="w-100 home_banner_img d-flex justify-content-center align-items-center">
           <img
-            src="https://res.cloudinary.com/dol1ba0ld/image/upload/v1696719511/Portafolio/Banner/pexels-pixabay-259091_cza1yq.jpg"
+            src="https://res.cloudinary.com/dol1ba0ld/image/upload/v1697481302/Portafolio/Banner/Diseño_sin_título_tkaddx.jpg"
             alt="banner"
           />
-          <p className="text-light"> Mauro <span className="text-danger">Villagra</span></p>
+          <p className="text-light"> Mauro <span className="text-primary">Villagra</span></p>
         </div>
       </div>
     </>
