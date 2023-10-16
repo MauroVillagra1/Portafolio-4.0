9f1396461a0e0c8c6604be68ddfc1bc13b2cdd09
Mauro Villagra <maurovillagra257@gmail.com>
2023-10-16 16:39:57 -0300

    Agregue informacion sobre la card full stack

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Banner from './banner/Banner'
+import "./Home.css"
+import Cards_programacion_web from './cards_programacion_web/cards_programacion_web'
+
 
 function Home() {
   return (
     <>
-    <div>
+    <div className='home'>
         <Banner></Banner>
+        <Cards_programacion_web></Cards_programacion_web>
     </div>
     </>
   )
